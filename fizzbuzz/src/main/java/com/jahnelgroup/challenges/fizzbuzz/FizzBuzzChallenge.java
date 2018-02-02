3595545f767ea77071ea9cd60e6cf337ff8db1a0
szgaljic <Steven Zgaljic>
2018-02-01 23:13:25 -0600

    Commiting FizzBuzz challenge

---
+++ fizzbuzz/src/main/java/com/jahnelgroup/challenges/fizzbuzz/FizzBuzzChallenge.java
@@ -0,0 +1,29 @@
+package com.jahnelgroup.challenges.fizzbuzz;
+
+/**
+ * Fizz Buzz Challenge
+ *
+ * Write a program that prints the numbers from 1 to 100.
+ *
+ * For multiples of three print "Fizz" instead of the number
+ * and for the multiples of five print "Buzz".
+ *
+ * For numbers which are multiples of both three and five print "FizzBuzz".
+ *
+ */
+public class FizzBuzzChallenge {
+
+    public static void main(String[] args) {
+        System.out.println("======================================");
+        System.out.println("Challenge: Fizz Buzz");
+        System.out.println("======================================");
+        new FizzBuzzChallenge().runFizzBuzz();
+    }
+
+    void runFizzBuzz(){
+
+        // write your logic here
+
+    }
+
+}
